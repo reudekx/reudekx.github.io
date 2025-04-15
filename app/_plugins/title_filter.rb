4f5e3089b79206bf825f289bb074b7d5436b6108
reudekx <reudekx@gmail.com>
2025-04-15 16:15:48 +0900

    feat: modify to use filename as title by default

--- app/_plugins/title_filter.rb
+++ app/_plugins/title_filter.rb
@@ -4,13 +4,11 @@ module Jekyll
       page = @context.registers[:page]
 
       title = %(<h1><a href="#{page["link"]}">)
-
-      title += if page["title"]
-        page["title"]
-      elsif page["name"]
+      
+      title += if page["name"]
         File.basename(page["name"], ".*")
       else
-        "무제"
+        page["title"]
       end
 
       title += %(</a></h1>)
